fe8dd5a45e0e8db7e7f57323a2cc03e0d64c64ea
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-08 23:45:17 -0500

    feat: create async use case base type

---
+++ lib/domain/use_cases/types.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+
+abstract class FutureUseCase<FailureType, ReturnType, ParamsType> {
+  const FutureUseCase();
+
+  Future<Either<FailureType, ReturnType>> call(ParamsType params);
+}
+
+class NoParams {
+  const NoParams();
+}
