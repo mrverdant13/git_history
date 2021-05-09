bbff0d15ea7a6c24265c6bb0b22b35cb3cf778a8
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-08 23:02:01 -0500

    feat: add app router

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
 import '../core/flavors.dart';
-import 'screens/home/screen.dart';
+import 'routers/app_router.gr.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final appRouter = AppRouter();
+    return MaterialApp.router(
       title: 'Flutter App Template (${kAppFlavor.tag})',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      routerDelegate: appRouter.delegate(),
+      routeInformationParser: appRouter.defaultRouteParser(),
     );
   }
 }
