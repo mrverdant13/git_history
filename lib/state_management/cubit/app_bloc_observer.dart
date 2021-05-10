71b568125f4e425b80e48beab9ec600e752fc362
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-10 03:52:33 -0500

    chore: log stacktrace on bloc errors

--- lib/state_management/cubit/app_bloc_observer.dart
+++ lib/state_management/cubit/app_bloc_observer.dart
@@ -11,7 +11,10 @@ class AppBlocObserver extends BlocObserver {
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    log(
+      'onError(${bloc.runtimeType}, $error)',
+      stackTrace: stackTrace,
+    );
     super.onError(bloc, error, stackTrace);
   }
 }
