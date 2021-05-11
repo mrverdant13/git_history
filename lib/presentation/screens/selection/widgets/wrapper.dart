2b6817a27b8254fc8075fbf46a69240f53571950
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-10 21:59:03 -0500

    feat: create selection screen wrapper
    - Inject selection form group in the widget tree
    
    - Inject users getter cubit in the widget tree

---
+++ lib/presentation/screens/selection/widgets/wrapper.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/dependency_injection.dart';
+import '../../../../state_management/cubit/users_getter/users_getter_cubit.dart';
+import '../state_management/selection_form_group/form_group.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({
+    Key? key,
+    required this.builder,
+  }) : super(key: key);
+
+  final WidgetBuilder builder;
+
+  @override
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          Provider(
+            create: (_) => SelectionFormGroup(),
+          ),
+          BlocProvider<UsersGetterCubit>(
+            create: (context) => getIt(),
+          ),
+        ],
+        builder: (context, child) => builder(context),
+      );
+}
