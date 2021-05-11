ee4cd74e3b3ccc068b143e712b172028c01fc82e
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-11 03:26:13 -0500

    feat: provide global store for the entire app

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../core/flavors.dart';
+import '../state_management/change_notifier/global_store/global_store_change_notifier.dart';
 import 'routers/app_router.gr.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,13 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appRouter = AppRouter();
-    return MaterialApp.router(
-      title: 'Flutter App Template (${kAppFlavor.tag})',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => GlobalStoreChangeNotifier(),
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Flutter App Template (${kAppFlavor.tag})',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routerDelegate: appRouter.delegate(),
+        routeInformationParser: appRouter.defaultRouteParser(),
       ),
-      routerDelegate: appRouter.delegate(),
-      routeInformationParser: appRouter.defaultRouteParser(),
     );
   }
 }
