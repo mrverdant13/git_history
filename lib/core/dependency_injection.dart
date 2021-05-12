dcd2efb8b814493325c87c3d080e092a6189620a
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-11 23:08:56 -0500

    build: inject dependencies for repo search

--- lib/core/dependency_injection.dart
+++ lib/core/dependency_injection.dart
@@ -1,11 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import '../domain/facades/repositories_repo/facade.dart';
 import '../domain/facades/users_repo/facade.dart';
+import '../domain/use_cases/get_repositories/use_case.dart';
 import '../domain/use_cases/get_users/use_case.dart';
+import '../infrastructure/data_source/repositories/remote/implementation.dart';
+import '../infrastructure/data_source/repositories/remote/interface.dart';
 import '../infrastructure/data_source/users/remote/implementation.dart';
 import '../infrastructure/data_source/users/remote/interface.dart';
+import '../infrastructure/facades/repositories_repo.dart';
 import '../infrastructure/facades/users_repo.dart';
+import '../state_management/cubit/repositories_getter/repositories_getter_cubit.dart';
 import '../state_management/cubit/users_getter/users_getter_cubit.dart';
 import 'flavors.dart';
 
@@ -29,11 +35,21 @@ Future<void> injectDependencies(Flavor flavor) async {
       dio: getIt(),
     ),
   );
+  getIt.registerLazySingleton<RepositoriesRDS>(
+    () => RepositoriesRDSImp(
+      dio: getIt(),
+    ),
+  );
 
   // Facades
   getIt.registerLazySingleton<UsersRepo>(
     () => UsersRepoImp(
       usersRDS: getIt(),
+    ),
+  );
+  getIt.registerLazySingleton<RepositoriesRepo>(
+    () => RepositoriesRepoImp(
+      repositoriesRDS: getIt(),
     ),
   );
 
@@ -43,10 +59,21 @@ Future<void> injectDependencies(Flavor flavor) async {
       usersRepo: getIt(),
     ),
   );
+  getIt.registerLazySingleton<GetRepositoriesByNameAndOwner>(
+    () => GetRepositoriesByNameAndOwner(
+      repositoriesRepo: getIt(),
+    ),
+  );
 
+  // State managers
   getIt.registerFactory<UsersGetterCubit>(
     () => UsersGetterCubit(
       getUsersByUsername: getIt(),
+    ),
+  );
+  getIt.registerFactory<RepositoriesGetterCubit>(
+    () => RepositoriesGetterCubit(
+      getRepositoriesByNameAndOwner: getIt(),
     ),
   );
 }
