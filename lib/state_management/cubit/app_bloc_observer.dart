f269cf59140da2ecf32d3cc2d41081d5ba60b202
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-11 22:56:01 -0500

    style: format bloc observer logger

--- lib/state_management/cubit/app_bloc_observer.dart
+++ lib/state_management/cubit/app_bloc_observer.dart
@@ -6,15 +6,33 @@ class AppBlocObserver extends BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    log('onChange(${bloc.runtimeType}, $change)');
+
+    final changeStrBuffer = StringBuffer()
+      ..writeln('State changed')
+      ..writeln('Bloc: ${bloc.runtimeType}')
+      ..writeln('Previous: ${change.currentState}')
+      ..writeln('New: ${change.nextState}');
+
+    log(
+      changeStrBuffer.toString(),
+      name: 'Bloc',
+    );
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    final errorStrBuffer = StringBuffer()
+      ..writeln('Error detected')
+      ..writeln('Bloc: ${bloc.runtimeType}')
+      ..writeln('Error: $error');
+
     log(
-      'onError(${bloc.runtimeType}, $error)',
+      errorStrBuffer.toString(),
+      error: error,
       stackTrace: stackTrace,
+      name: 'Bloc',
     );
+
     super.onError(bloc, error, stackTrace);
   }
 }
