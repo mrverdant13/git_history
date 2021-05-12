c084fd00f99ba45938c9b0f586eb473b94ff7d3c
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-11 23:34:57 -0500

    feat: create git commit history screen

---
+++ lib/presentation/screens/git_commits_history/screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GitCommitHistoryScreen extends StatelessWidget {
+  const GitCommitHistoryScreen();
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        body: Center(
+          child: Text('Git Commit History'),
+        ),
+      );
+}
