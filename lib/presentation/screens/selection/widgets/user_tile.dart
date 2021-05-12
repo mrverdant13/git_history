ffcca6821e696d26ecf246fa7b7c280631ec419a
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-11 23:13:27 -0500

    feat: switch search mode on user selection
    - Disable username field on user selection
    
    - Enable repo name field on user selection

--- lib/presentation/screens/selection/widgets/user_tile.dart
+++ lib/presentation/screens/selection/widgets/user_tile.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../../../../domain/entities/user/entity.dart';
 import '../../../../state_management/change_notifier/global_store/global_store_change_notifier.dart';
+import '../state_management/selection_form_group/form_group.dart';
 import '../values/dimensions.dart';
 
 class UserTile extends StatelessWidget {
@@ -20,6 +21,10 @@ class UserTile extends StatelessWidget {
         title: Text(user.username),
         onTap: () {
           context.read<GlobalStoreChangeNotifier>().selectedUser = user;
+
+          final formGroup = context.read<SelectionFormGroup>();
+          formGroup.usernameTextControl.markAsDisabled();
+          formGroup.repositoryNameTextControl.markAsEnabled();
         },
       );
 }
