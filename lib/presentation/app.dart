14e48428313fee1df7690775105cfd17daece603
Karlo Verde <karlo.verde.1996@gmail.com>
2021-05-13 00:19:13 -0500

    build: setup app localization

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
 import '../core/flavors.dart';
@@ -24,6 +25,8 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
         routerDelegate: context.read<AppRouter>().delegate(),
         routeInformationParser: context.read<AppRouter>().defaultRouteParser(),
       ),
